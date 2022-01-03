66c21bb6fda75535726f0d2ccdc22b9c3d7a4ead
omroncn <74035982+omroncn@users.noreply.github.com>
2022-01-03 10:53:41 +0800

    6

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,7 +31,9 @@ cat << EOF > ${DIR_TMP}/heroku.json
 EOF
 
 # Get V2Ray executable release
-curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/${VER}/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
+downloadurl="github.com/v2fly/v2ray-core/releases/${VER}/download/v2ray-linux-64.zip"
+echo $downloadurl
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL ${downloadurl} -o ${DIR_TMP}/v2ray_dist.zip
 busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
 
 # Convert to protobuf format configuration
